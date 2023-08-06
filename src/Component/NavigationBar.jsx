e2a9b73be4b577532725425ae619741812d3f9b0
unknown <rakibulhasan.seu.cse@gmail.com>
2023-08-06 18:06:41 +0600

    Responsive issue solved

--- src/Component/NavigationBar.jsx
+++ src/Component/NavigationBar.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Container, Nav, Navbar } from "react-bootstrap";
+import { Button, Container, Nav, Navbar } from "react-bootstrap";
 import logo from "../assets/logo.png";
 import { BsMoon, BsSun } from "react-icons/bs";
 
@@ -27,8 +27,8 @@ const NavigationBar = () => {
         } sticky-top mb-5 mb-lg-0 borderBottom`}
       >
         <Container>
-          <Navbar.Brand className="fw-bold fs-2" href="#home">
-            <img className="img-fluid w-25" src={logo} alt="" />
+          <Navbar.Brand className="fw-bold" href="#home">
+            <img className="img-fluid" style={{width: "4.9rem"}} src={logo} alt="" />
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
@@ -45,13 +45,15 @@ const NavigationBar = () => {
               <Nav.Link className="mx-2 fw-semibold nav-hover" href="#contact">
                 Contact
               </Nav.Link>
-              <button
-                className="btn btn-primary rounded-circle"
+            </Nav>
+            <div className="d-flex">
+              <Button
+                className="ms-2 rounded-circle"
                 onClick={handleThemeChange}
               >
-                {theme === "light" ? <BsMoon /> : <BsSun />}
-              </button>
-            </Nav>
+                {theme == "light" ? <BsMoon /> : <BsSun />}
+              </Button>
+            </div>
           </Navbar.Collapse>
         </Container>
       </Navbar>
